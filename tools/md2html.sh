3e8ec2cf01a3a0c5e03b0ace0e9e341f92e14227
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2024-12-26 00:01:24 +0100

    tools/md2html.sh: merging and development, p.2
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

--- tools/md2html.sh
+++ tools/md2html.sh
@@ -9,11 +9,25 @@ li_B=".<span style='visibility: hidden;'>_</span></b>"
 ul_A="<ul class='dqt'><li class='dqt'><blockquote class='dqt'>"
 ul_B="</blockquote></li></ul>"
 
+#function orig_mdlinkconv() {
+#    sed -e "s,\([ []*\)\[\([^][]*\)\]\([^(]\),\\1\&lbrack;\\2\&rbrack;\\3,g;" \
+#        -e "s,\[\([^[]*\)](\([^)]*\)),<a href='\\2'>\\1</a>,g" \
+#        -e "s,\&lbrack;,[,g" -e "s,\&rbrack;,],g" "$@"
+#}
+
+function mini_mdlinkconv() {
+    sed -e "s,\([ []*\)\[\([^][]*\)\]\([^(]\),\\1\&lbrack;\\2\&rbrack;\\3,g;" \
+        -e "s,\[\([^[]*\)](\([^)]*\)),<a href=\"\\2\">\\1</a>,g" "$@"
+}
+
+function full_mdlinkconv() {
+    mini_mdlinkconv -e "s,\&lbrack;,[,g" -e "s,\&rbrack;,],g" "$@"
+}
+
 function md2htmlfunc() {
-    local i str=$(basename ${2%.html}) idx="" dir=""
+    local i str=$(basename ${2%.html}) dir=""
     test "$str" == "index" && dir="html/"
-    test "$str" == "index" && idx="idx"
-    #echo "str:$str idx:$idx dir:$dir" >&2
+
     echo -n "<!DOCTYPE html>
 <html>
     <head>
@@ -24,11 +38,11 @@ function md2htmlfunc() {
     </head>
     <body>
 " >$2
-    if [ -n "$idx" ]; then
+    if [ "$str" = "index" ]; then
         sed -e "s, - (\[...raw...\]([^)]*\.md)) , - ," $1
     else
         cat $1
-    fi >>$2
+    fi | full_mdlinkconv >>$2
     sed -e "s,@,\&commat;,g" -e "s,°,\&deg;,g" \
 -e "s,m\*rda,m\&astr;rda,g" -e "s,sh\*t,sh\&astr;t,g" \
 -e "s,c\*zzo,c\&astr;zzo,g" -e "s,d\*ck,d\&astr;ck,g" \
@@ -44,7 +58,6 @@ function md2htmlfunc() {
 -e "s,^> \(.*\),<blockquote>\\1</blockquote>," \
 -e "s,^\( *\)[-+\*] \(.*\),\\1<li>\\2</li>," \
 -e "s,^\( *\)\([0-9]*\)\. \(.*\),\\1${li_A}\\2${li_B}\\3</li>," \
--e 's,\(\[*\)\[\([^]]*\)\](\([^)]*\)),\1<a href="\3">\2<\/a>,g' \
 -e "s,\\\<\(.*\)\\\>,\&lt;\\1\&gt;,g" \
 -e "s,^ *$,<p/>," -e "s,^---.*,<hr>," -i $2
 
@@ -83,21 +96,7 @@ function md2htmlfunc() {
     </body>
 </html>" >> $2
 
-    if [ -z "$idx" ]; then
-        sed -e "s,\( src=.\)\([^ ]*\.png\),\\1../\\2,g"  \
-            -e "s,\( src=.\)\([^ ]*\.jpg\),\\1../\\2,g"  \
-            -e "s,\( href=.\)\([^ ]*\.md\),\\1../\\2,g"  \
-            -e "s,\( href=.\)\([^ ]*\.pdf\),\\1../\\2,g" \
-            -e "s,\( href=.\)\.\./\(http.://\),\\1\\2,g" \
-            -e "s,\( href=.\)\.\./\([^ ]*\)\.md,\\1\\2.html,g" \
-            -e "s,\( href=.\)italian/\([^ ]*.html\),\\1../italian/html/\\2,g" \
-            -e "s,\( href=.\)html/,\\1,g" -i $2
-    elif pwd | grep -q "chatbots-for-fun"; then
-        sed -e "s,\( href=.\)\([^ ]*\)\.md,\\1html/\\2.html,g" \
-            -e "s,\( href=.\)html/\(http[^ ]*\)\.html,\\1\\2.md," -i $2
-    fi
     sed -e "s/<a [^>]*href=.http[^>]*/& target='_blank'/g" -i $2
-
     for i in 3 2 1; do
         let b=i*3 a=b-2 c=i+1; a=${a/1/2}; #echo "$i $a $b $c" >&2
         sed -e "s/ \{$a,$b\}<li\([ >]\)/<li class='li${c}in'\\1/" -i $2
@@ -141,8 +140,18 @@ done
 
 echo
 echo "redirecting html links ..."
+
+function get_images_list() {
+    local dir ext
+    for dir in "data/" "img/" ""; do
+        for ext in jpg png; do
+            ls -1 ${dir}*.${ext}
+        done 2>/dev/null
+    done
+}
+
 for j in $list; do
-    for i in $(ls -1 img/*.png img/*.jpg *.png *.jpg 2>/dev/null); do
+    for i in $(get_images_list); do
         sed -e "s,\(href=.\)$i,\\1../$i,g" \
             -e "s,\(src=.\)$i,\\1../$i,g" -i $j
     done
@@ -151,12 +160,22 @@ for j in $list; do
     done
 done
 
-if [ $index -eq 1 ]; then
-    i="README.md"
-    echo "converting $i in index.html ..."
-    md2htmlfunc "$i" index.html
-    list="$list index.html"
-fi
+function link_md2html() {
+    local i=${1%.md} f="$2" dir=""
+    test "$f" == "index.html" && dir="html/"
+    sed -e "s,\(href=[\"']\)$i\.md\([\"']\),\\1${dir}$i.html\\2,g" -i $f
+}
+
+for i in *.md; do
+    #i=${i%.md}
+    #echo "$i" >&2
+    test "$i" == "template.md" && continue
+    for j in html/*.html index.html; do
+        #sed -e "s,\(href=[\"']\)$i\.md\([\"']\),\\1$i.html\\2,g" -i $j
+        link_md2html $i $j
+    done
+    #sed -e "s,\(href=[\"']\)$i\.md\([\"']\),\\1html/$i.html\\2,g" -i index.html
+done
 
 echo
 echo "converting md tables ..."
