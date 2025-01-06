5390fdfa1d3bf92c532779cbfa54181907c50e82
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2025-01-06 06:24:12 +0100

    html/tools: whole website aesthetic redesigning
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

--- tools/ptopbar.sh
+++ tools/ptopbar.sh
@@ -20,9 +20,9 @@ fi
 
 LINE_SHADE="tpbr${color}"
 TEXT_SHADE="tpbrtext"
-LINE_MARK="&#9783;&nbsp;<b>&Ropf;</b>"
+LINE_MARK="<b>&#9783;&thinsp;&Ropf;</b>"
 LINE_DASH="&nbsp;&mdash;&nbsp;"
-LANG_DASH="&middot;"
+MDOT_DASH="&nbsp;<b>&middot;</b>&nbsp;"
 
 REVISION_STRING=""
 PUBLISH_UNIVDATE=""
@@ -32,11 +32,11 @@ PUBLISH_LINK=""
 declare -A PROJ_LINKS GOTO_LINKS
 
 PROJ_LINKS[1,1]="../chatbots-for-fun/index.html"
-PROJ_LINKS[1,2]="<tt>C4F</tt>"
+PROJ_LINKS[1,2]="C4F"
 PROJ_LINKS[2,1]="../roberto-a-foglietta/index.html"
-PROJ_LINKS[2,2]="<tt>RAF</tt>"
+PROJ_LINKS[2,2]="RAF"
 PROJ_LINKS[3,1]="../chatgpt-answered-prompts/index.html"
-PROJ_LINKS[3,2]="<tt>Q&A</tt>"
+PROJ_LINKS[3,2]="Q&A"
 
 ################################################################################
 
@@ -59,7 +59,8 @@ for i in 1 2 3; do
     let n++
 done
 GOTO_LINKS[1,1]="index.html#pages-index"
-GOTO_LINKS[1,2]="&#x2798;&#8505;."
+GOTO_LINKS[1,2]="&#x2799;&hairsp;&#8505;&thinsp;&equiv;"
+#GOTO_LINKS[1,2]="&#x2798;&hairsp;&#8505;&thinsp;&equiv;"
 
 if grep -qie "^#\{1,3\} *index *$" README.md 2>/dev/null; then
     GOTO_LINKS[1,1]="index.html#index"
@@ -78,13 +79,14 @@ declare -A LANG_LINKS
 local str lg LG lang=${7:-auto} trsl=0 file=${6:-}
 
 PUBLISH_UNIVDATE="${3:-$(date +%F)}"
+ALINK_CLASS="class='${LINE_SHADE} topbar'"
 
 ORIGIN_CODE=""
 PUBLISH_SOURCE="${4:-}"
 if [ -n "$PUBLISH_SOURCE" ]; then
     PUBLISH_LINK="${5:-}"
-    ORIGIN_CODE=" ${LINE_DASH} origin:&nbsp; <b class='tpbrbold tpbrlink'>
-<a class='${LINE_SHADE}' href='${PUBLISH_LINK}'>${PUBLISH_SOURCE}</a></b>"
+    ORIGIN_CODE=" ${LINE_DASH} origin:&nbsp; 
+<a $ALINK_CLASS href='${PUBLISH_LINK}'>${PUBLISH_SOURCE}</a>"
 fi
 
 declare -i skip=$lang
@@ -92,18 +94,18 @@ test $skip -ne 0 && lang="auto"
 #echo "7: '$7', skip: '$kip', lang: '$lang'" >&2
 if [ -n "$file" -a "$skip" != "-99" ]; then
     lang=${lang,,}
-    TRNSL_STRN="<b class='tpbrlang tpbrbold tpbrlink'>"
+    TRNSL_STRN=""
     for LG in IT EN DE FR ES; do
         let skip++; test $skip -gt 0 || continue; lg=${LG,,}
         if [ "$lang" != "$lg" ]; then
-            TRNSL_STRN+="<tt class='tpbrlang'><a class='${LINE_SHADE}' "
+            TRNSL_STRN+="<a $ALINK_CLASS "
             str=$(print_transl_from_to "$6" $lang $lg $LG)
-            TRNSL_STRN+="href='$str'>${LG}</a></tt>"
-            if [ "$LG" != "ES" ]; then TRNSL_STRN+=" ${LANG_DASH} "; fi
+            TRNSL_STRN+="href='$str'>${LG}</a>"
+            if [ "$LG" != "ES" ]; then TRNSL_STRN+=" ${MDOT_DASH} "; fi
             trsl=1
         fi
     done
-    TRNSL_STRN+="</b>"
+    #TRNSL_STRN+="</b>"
     #TRNSL_STRN="${TRNSL_STRN%</tt>*}</tt></b>"
 fi
 if [ "$trsl" == "1" ]; then
@@ -112,9 +114,10 @@ else
     TRNSL_STRN=""
 fi
 
-TOPBAR_STRING="<br/><div class='topbar ${LINE_SHADE} ${TEXT_SHADE}'>&nbsp;"\
+TOPBAR_STRING="
+<div class='topbar ${LINE_SHADE} ${TEXT_SHADE}'>&nbsp;"\
 "${LINE_MARK} ${LINE_DASH} published:&nbsp; <b class='tpbrbold'>"\
-"${PUBLISH_UNIVDATE}</b>${REVISION_STRING}${ORIGIN_CODE}${TRNSL_STRN}"
+"${PUBLISH_UNIVDATE}</b> ${REVISION_STRING}${ORIGIN_CODE}${TRNSL_STRN}"
 
 gotolist="1 2 3"
 if [ "$file" != "index.html" ]; then
@@ -122,13 +125,13 @@ if [ "$file" != "index.html" ]; then
     GOTO_LINKS[1,2]=".&#x27F0;."
 fi
 
-TOPBAR_STRING+=" ${LINE_DASH} goto:&nbsp;<b class='tpbrbold tpbrlink'>"
+TOPBAR_STRING+=" ${LINE_DASH} goto:&nbsp;"
 for i in $gotolist; do
-    TOPBAR_STRING+=" <a class='${LINE_SHADE}' href='${GOTO_LINKS[$i,1]}'"\
+    TOPBAR_STRING+=" <a $ALINK_CLASS href='${GOTO_LINKS[$i,1]}'"\
 "$(test $i -ne 1 && printf target='_blank')>${GOTO_LINKS[$i,2]}</a>"
-    if [ $i -lt 3 ]; then TOPBAR_STRING+=" ${LANG_DASH}"; fi
+    if [ $i -lt 3 ]; then TOPBAR_STRING+=" ${MDOT_DASH}"; fi
 done
-echo "${TOPBAR_STRING}</b>&nbsp;</div>"
+echo "${TOPBAR_STRING}&nbsp;</div>"
 
 } ##############################################################################
 
@@ -164,9 +167,9 @@ revnum+=$(echo "$gitlog" | grep . | wc -l)
 command git status -s "$file" | grep -q . && let revnum++
 
 if [ $revnum -gt 0 ]; then
-    REVISION_STRING=" ${LINE_DASH} revision: <b class='tpbrbold'
->${revnum}</b
->"
+    REVISION_STRING=" ${LINE_DASH} revision: <b class='tpbrbold
+'>${revnum}</b class='
+'>"
 fi 2>/dev/null
 
 date1st=${1:-}
