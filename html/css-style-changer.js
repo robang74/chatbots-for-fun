debc96fe0b23ce29c5d43f8fd9fffb1ecafa9a11
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2025-01-09 09:24:10 +0100

    html/css-style-changer.js: themes correct order restored
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

--- html/css-style-changer.js
+++ html/css-style-changer.js
@@ -2,8 +2,8 @@
 <!--html: funcname='nextStylesheet', linkto='menu', linkfor='onClick' //-->
 
 const stylesheets = [`${cssdir}colours.css`, `${cssdir}webdark.css`,
-    `${cssdir}pureblk.css`, `${cssdir}bwprint.css`, `${cssdir}onpaper.css`,
-    `${cssdir}pdfgray.css`, `${cssdir}pdfcool.css`, `${cssdir}pdfwarm.css`];
+    `${cssdir}pureblk.css`, `${cssdir}bwprint.css`, `${cssdir}pdfgray.css`,
+    `${cssdir}onpaper.css`, `${cssdir}pdfcool.css`, `${cssdir}pdfwarm.css`];
 
 let currentIndex = 0;
 
