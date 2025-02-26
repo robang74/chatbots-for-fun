0256806c6656314b72bcb31f71c596e869525fe1
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2025-02-26 11:01:17 +0100

    aisp/doprompt.sh: generating SoNIa/AleX prompts from text files
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

---
+++ aisp/doprompt.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -e
+
+function mainfunc() {
+    local x vers
+    if echo "${1:-}" | grep -qie "^AleX$"; then
+        type="peer-review"
+    elif echo "${1:-}" | grep -qie "^SoNia$"; then
+        type="brain-storm"
+    else
+        type=${1:-peer-review}
+    fi
+    x="\n---\n"
+    vers=$(cat aisp/$type/ver.txt)
+    printf "%s %s\n\n" "$(cat aisp/$type/tle.txt)" $vers
+    printf "%s\n$x\n"  "$(cat aisp/$type/aim.txt)"
+    printf "%s\n\n\n"  "$(cat aisp/doc-dealing/rts.txt)"
+    printf "%s\n\n\n"  "$(cat aisp/doc-dealing/ops.txt)"
+    printf "%s\n\n\n"  "$(cat aisp/doc-dealing/rag.txt)"
+    cat aisp/$type/prf.txt 2>/dev/null && printf "\n\n\n"
+    cat aisp/$type/chr.txt
+}
+
+if [ "x${1:-}" == "x-a" ]; then
+    mainfunc alex  2>/dev/null
+    mainfunc sonia >&2
+else
+    mainfunc "${1:-}"
+fi
+
